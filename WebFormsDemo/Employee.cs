e0e5da4d2f71c75ae033618c2c4e4b870a879aa4
Ghanshyam Mistry <gmistry1@nait.ca>
2020-06-09 21:16:49 -0600

    need work

--- WebFormsDemo/Employee.cs
+++ WebFormsDemo/Employee.cs
@@ -7,5 +7,20 @@ namespace WebFormsDemo
 {
     public class Employee
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public Employee()
+        { 
+        }
+
+        public Employee(int id, string name, string phone)
+        {
+            ID = id;
+            Name = name;
+
+        }
+
+
     }
 }
